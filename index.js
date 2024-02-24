e475a694743793aaadec005b54c5def034f81b8d
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-02-25 01:54:55 +1100

    Add cooldown functionality to commands

--- index.js
+++ index.js
@@ -129,6 +129,9 @@ client.on("interactionCreate", async (interaction) => {
     });
   }
 
+
+
+
   if (interaction.isCommand()) {
     const command = interaction.client.commands.get(interaction.commandName);
 
@@ -138,6 +141,29 @@ client.on("interactionCreate", async (interaction) => {
       );
       return;
     }
+
+    const { cooldowns } = interaction.client;
+
+    if (!cooldowns.has(command.data.name)) {
+      cooldowns.set(command.data.name, new Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.data.name);
+    const defaultCooldownDuration = 3;
+    const cooldownAmount = (command.cooldown ?? defaultCooldownDuration) * 1_000;
+
+    if (timestamps.has(interaction.user.id)) {
+      const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+      if (now < expirationTime) {
+        const expiredTimestamp = Math.round(expirationTime / 1_000);
+        return interaction.reply({ content: `Please wait, you are on a cooldown for \`${command.data.name}\`. You can use it again <t:${expiredTimestamp}:R>.`, ephemeral: true });
+      }
+    }
+
+    timestamps.set(interaction.user.id, now);
+    setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
 
     if (command.permission) {
       if (
@@ -355,7 +381,7 @@ client.on("guildCreate", async (guild) => {
     );
   }
 
-    setupServerdata(guildid);
+  setupServerdata(guildid);
 });
 
 client.on("messageDelete", async (message) => {
