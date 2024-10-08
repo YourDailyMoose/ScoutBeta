a0c95310bc287ee26ec9148e16fd8175f8086fe0
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-10-08 22:10:28 +1100

    Refactor import statements in addxp.js

--- commands/levels/addxp.js
+++ commands/levels/addxp.js
@@ -1,4 +1,4 @@
-const [ SlashCommandBuilder, EmbedBuilder ] = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { getGuildBotColours } = require('../../database');
 const { addUserXP, getUserXP } = require('../../database');
 
