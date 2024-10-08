eda7bc27a0ab741fd2130362d0e4e910b6484cab
YourDailyMoose <90841132+YourDailyMoose@users.noreply.github.com>
2024-10-08 22:29:05 +1100

    Refactor addxp.js to improve error handling and messaging

--- commands/levels/removexp.js
+++ commands/levels/removexp.js
@@ -1,33 +1,33 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { getGuildBotColours } = require('../../database');
+const { getGuildBotColours, removeUserXP } = require('../../database');
 const { addUserXP, getUserXP } = require('../../database');
 
 module.exports = {
     cooldown: 5,
     data: new SlashCommandBuilder()
-        .setName('addxp')
-        .setDescription('Add XP to a user')
+        .setName('removexp')
+        .setDescription('Remove XP from a user')
         .addUserOption(option =>
         option.setName('user')
             .setDescription('The user you want to add XP to')
             .setRequired(true))
         .addIntegerOption(option =>
         option.setName('xp')
-            .setDescription('The amount of XP to add')
+            .setDescription('The amount of XP to remove')
             .setRequired(true)),
     async execute(interaction) {
         const guildColours = await getGuildBotColours(interaction.guild.id);
         const user = interaction.options.getUser('user');
         const xp = interaction.options.getInteger('xp');
         const userXP = await getUserXP(interaction.guild.id, user.id);
-        const addXP = addUserXP(interaction.guild.id, user.id, xp);
-        if (!addXP) {
+        const removeXP = removeUserXP(interaction.guild.id, user.id, xp);
+        if (!removeUserXP) {
             return interaction.reply({
                 embeds: [
                     new EmbedBuilder()
                     .setColor(guildColours.error)
                     .setTitle(`Error`)
-                    .setDescription(`An error occurred. Please try again or contact support.`)
+                    .setDescription(`User has no XP, user does not have enough XP to remove, or an error occurred. Please try again or contact support.`)
                     .setTimestamp()
                 ]
             });
@@ -37,7 +37,7 @@ module.exports = {
             new EmbedBuilder()
             .setColor(guildColours.success)
             .setTitle(`Success`)
-            .setDescription(`Added ${xp} XP to ${user}! They now have ${userXP + xp} XP.`)
+            .setDescription(`Removed ${xp} XP from ${user}! They now have ${userXP - xp} XP.`)
             .setTimestamp()
         ]
         });
